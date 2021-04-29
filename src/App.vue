93e96e650b082bafbe5888c32a69ec27c6310314
mfcbentes <mfcbentes.stm@gmail.com>
2021-04-28 23:47:14 -0300

    Criação de textArea e Checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Cadastro</h1>
+    <div>
+      <form action="">
+        <label for="">Nome </label>
+        <input type="text" v-model="user.nome" /><br /><br />
+
+        <label for="">E-mail </label>
+        <input type="email" v-model="user.email" /><br /><br />
+
+        <label for="">Qtde funcionários </label>
+        <input type="number" v-model="user.qtdeFunc" /><br /><br />
+
+        <label for="">Opinião </label>
+        <textarea v-model="opiniao"></textarea><br /><br />
+
+        <label for="">Tecnologias</label>
+        <span>
+          <input type="checkbox" value="html" v-model="tecnologias" />HTML
+        </span>
+        <span>
+          <input type="checkbox" value="css" v-model="tecnologias" />CSS
+        </span>
+        <span>
+          <input
+            type="checkbox"
+            value="javascript"
+            v-model="tecnologias"
+          />Javascript
+        </span>
+        <span>
+          <input type="checkbox" value="vuejs" v-model="tecnologias" />VueJS
+        </span>
+      </form>
+    </div>
+    <hr />
+    <div class="item">
+      <label for="">Nome: {{ user.nome }}</label> <br /><br />
+      <label for="">E-mail: {{ user.email }}</label> <br /><br />
+      <label for="">Qtde funcionários: {{ user.qtdeFunc }}</label> <br /><br />
+      <label for="">Sua opinião: {{ opiniao }}</label> <br /><br />
+      <label for="">Tecnologias:</label>
+      <ul>
+        <li v-for="tech in tecnologias" :key="tech">{{ tech }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      user: {
+        nome: "",
+        email: "",
+        qtdeFunc: 0,
+      },
+      opiniao: "",
+      tecnologias: [],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
 }
 </style>
